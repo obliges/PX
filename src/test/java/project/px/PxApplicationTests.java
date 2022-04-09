71c30b2a9b440a45671272bb2b5ec208766dddb5
obliges <rlghks1710@kaist.ac.kr>
2022-04-09 16:38:11 +0900

    initial commit

---
+++ src/test/java/project/px/PxApplicationTests.java
@@ -0,0 +1,13 @@
+package project.px;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
